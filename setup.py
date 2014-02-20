525b72d6683b5d984bf05a72a46b7ace639753a8
Thomas Loiret <thomas.loiret@gmail.com>
2014-02-20 15:12:42 +0100

    Fix stupid bug

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ setup(
     install_requires=[
         'django>=1.5',
         'reportlab>=2.7',
-        'PyPDF2>=1.20'
-        'django-extensions>=1.3.3'
+        'PyPDF2>=1.20',
+        'django-extensions>=1.3.3',
     ]
 )
