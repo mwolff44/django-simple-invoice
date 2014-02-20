67f6e02d9515409dcce88eaa98c892ea321b70ab
Thomas Loiret <thomas.loiret@gmail.com>
2014-02-20 14:17:06 +0100

    Add a setup.py we can now install this app with pip

---
+++ setup.py
@@ -0,0 +1,48 @@
+import re
+from os import path
+from setuptools import setup, find_packages
+
+
+def read(*parts):
+    return open(path.join(path.dirname(__file__), *parts)).read()
+
+
+def find_version(*file_paths):
+    version_file = read(*file_paths)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                              version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
+
+setup(
+    name='simple-invoice',
+    version=find_version('invoice', '__init__.py'),
+    description='Invoicing for Django',
+    author='inextcom',
+    author_email='contact@inextcom.fr',
+    url='https://bitbucket.org/inextcom/django-simple-invoice',
+    packages=find_packages(),
+    zip_safe=False,
+    package_data={
+        'menu_manager': [
+            'locale/*/LC_MESSAGES/*',
+            'templates/invoice/*',
+            'static/*',
+        ],
+    },
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+    ],
+    install_requires=[
+        'django>=1.5',
+        'reportlab>=2.7',
+        'PyPDF2>=1.20'
+    ]
+)
