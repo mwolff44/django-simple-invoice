40dae7f3f34d39c31bb435867ed4258c557e179a
mwolff <mathiaswolff@mac.com>
2015-01-29 15:30:19 +0100

    add test settings

---
+++ invoice/test_settings.py
@@ -0,0 +1,38 @@
+import os
+import django
+
+if django.VERSION[:2] >= (1, 3):
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': ':memory:',
+        }
+    }
+else:
+    DATABASE_ENGINE = 'sqlite3'
+
+SITE_ID = 1
+
+MIDDLEWARE_CLASSES = (
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'axes.middleware.FailedLoginMiddleware'
+)
+
+ROOT_URLCONF = 'invoice.test_urls'
+
+INSTALLED_APPS = [
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.sites',
+    'django.contrib.messages',
+    'django.contrib.admin',
+
+    'invoice',
+]
+
+SECRET_KEY = 'too-secret-for-test'
+
+LOGIN_REDIRECT_URL = '/admin'
